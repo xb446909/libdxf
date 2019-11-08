9844a8b9f3e18fad59f092f970aa84ee2bb4702f
Xiong <xiongblw0852@hanslaser.com>
2019-11-08 12:12:32 +0800

    修复了释放时没有删除单例模式的bug，添加了小的测试例

---
+++ tester/tester.cpp
@@ -0,0 +1,19 @@
+﻿// tester.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "../libdxf/libdxf.h"
+
+#pragma comment(lib, "../Debug/libdxf.lib")
+
+int main()
+{
+	std::cout << "ReadDxf: " << (ReadDxf("test.dxf") ? "OK" : "NG") << std::endl;
+	std::cout << "GetShapeCount: " << GetShapeCount() << std::endl;
+	Release();
+	std::cout << "ReadDxf: " << (ReadDxf("test.dxf") ? "OK" : "NG") << std::endl;
+	std::cout << "GetShapeCount: " << GetShapeCount() << std::endl;
+	Release();
+}
+
